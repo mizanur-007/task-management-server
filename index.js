553665ca0db699283618fd3f882625eff4da3347
MD Mizanur Rahman <mizanridoy007@gmail.com>
2023-11-02 23:18:37 +0600

    data fetched through a id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -61,6 +61,14 @@ app.get("/", (req, res) => {
     catch{
         console.log("error")
     }
+  })
+
+  //task with id
+  app.get("/tasks/:id",async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId(id)};
+    const result = await tasksCollection.findOne(query);
+    res.send(result)
   })
 
   //cookie post with jwt
